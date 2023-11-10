6b211b7fef5b44d3600a991bef9b5bc144567fc3
wSeungMi <seungmi.dev@gmail.com>
2023-11-10 17:06:56 +0900

    Style: TodoInfo 페이지 스타일 구현

--- 03.react/react_todo/src/pages/info/TodoInfo.jsx
+++ 03.react/react_todo/src/pages/info/TodoInfo.jsx
@@ -1,14 +1,25 @@
+import Nav from "../../layout/Nav";
+import HeaderInfo from "../../layout/HeaderInfo";
+
 const TodoInfo = () => {
   return (
-    <div id="app" className="todoInfo">
-      <dl>
-        <dt className="todoListTitle">제목</dt>
-        <dd>TypeScript 공부</dd>
-        <dt className="todoListTitle">상세 내용</dt>
-        <dd className="detailContent">내용내용</dd>
-        <button className="modifyButton">수정하기</button>
+    <>
+      <Nav />
+      <HeaderInfo />
+      <dl className="w-[390px] h-[660px] my-0 mx-auto flex flex-col items-center bg-white">
+        <dt className="w-[336px] mt-[10px] mb-[10px] font-bold">제목</dt>
+        <dd className="w-[336px] min-h-[55px] ml-0 p-[6px] flex items-center bg-[#d9d9d9] font-bold rounded-[5px]">
+          TypeScript 공부
+        </dd>
+        <dt className="w-[336px] mt-[10px] mb-[10px] font-bold">상세 내용</dt>
+        <dd className="w-[336px] h-[400px] py-[12px] px-[6px] flex items-start bg-[#d9d9d9] font-bold rounded-[5px]">
+          내용내용
+        </dd>
+        <button className="w-[336px] h-[58px] mt-[32px] bg-[#383cc2] text-white text-lg rounded-[10px]">
+          수정하기
+        </button>
       </dl>
-    </div>
+    </>
   );
 };
 
