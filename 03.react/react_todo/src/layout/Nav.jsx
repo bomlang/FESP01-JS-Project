cb3316d4da0db2c447c8ce224fa170242fe2c1a3
wSeungMi <seungmi.dev@gmail.com>
2023-11-10 20:11:16 +0900

    Feat: Nav 뒤로가기 버튼 구현

--- 03.react/react_todo/src/layout/Nav.jsx
+++ 03.react/react_todo/src/layout/Nav.jsx
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
+  const navigate = useNavigate();
+
+  const handlePageBack = () => {
+    navigate(-1);
+  };
+
   return (
     <nav className="flex ml-[30px] mt-[20px] ">
-      <i className="mx-1.5">
+      <button className="mx-1.5" onClick={handlePageBack}>
         <FontAwesomeIcon
           icon={faArrowLeft}
           className="text-[25px] text-white font-bold"
         />
-      </i>
+      </button>
     </nav>
   );
 }
